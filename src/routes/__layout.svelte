88b977f0655d77031efd516b0a8ffa45d51adf78
Robin Drexler <drexler.robin@gmail.com>
2021-12-22 22:03:22 -0500

    attempt to remove service worker

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,7 +3,20 @@
 	import Nav from '$lib/Nav.svelte';
 	import Footer from '$lib/Footer.svelte';
 	import ContentWrapper from '$lib/layout/ContentWrapper.svelte';
-	import { base as basePath } from '$app/paths';
+
+	/**
+	 * remove old service workers that may still be lingering
+	 */
+	if ('serviceWorker' in navigator) {
+		navigator.serviceWorker.getRegistrations().then(function (registrations) {
+			if (!registrations.length) {
+				return;
+			}
+			for (let registration of registrations) {
+				registration.unregister();
+			}
+		});
+	}
 </script>
 
 <article class="grid grid-rows-[auto_1fr_auto] h-full gap-9">
